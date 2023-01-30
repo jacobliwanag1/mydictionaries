fee648f66b29efd6bbf9e1abe2a676846598a3d9
Jacob Liwanag <jacob_liwanag1@baylor.edu>
2023-01-30 15:46:29 -0600

    Week 1

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,16 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+#print out the name of the second child
+
+#print out the na,e of the cat
+
+#iterate through all children and print out each child
+
+
+#print out the pets in this format:
+# type of pet: dog name of pet: Fido
+
